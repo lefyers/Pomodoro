48717c97f135234ebeeb0df9eac50543de8656cd
Yersultan <ersultan2048@gmail.com>
2026-05-13 21:44:09 +0500

    fix database&cache accessor

--- database/database.py
+++ database/database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
+from settings import Settings
 
-# engine = create_engine("sqlite:///pomodoro.sqlite")
-engine = create_engine("postgresql+psycopg2://postgres:password@localhost:5432/pomodoro")
+settings = Settings()
+
+engine = create_engine(settings.db_url)
 
 Session = sessionmaker(engine)
 def get_db_session() -> Session:
