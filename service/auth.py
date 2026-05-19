63177905eaa1ef7a3fe28ec7615d98c330a42031
Yersultan <ersultan2048@gmail.com>
2026-05-19 20:59:44 +0500

    wip step 3 - google-auth

--- service/auth.py
+++ service/auth.py
@@ -8,7 +8,7 @@ from client.google import GoogleClient
 from exception import UserNotFoundException, UserNotCorrectPasswordException, TokenExpired, TokenNotCorrect
 from models import UserProfile
 from repository import UserRepository
-from schema import UserLoginSchema
+from schema import UserLoginSchema, UserCreateSchema
 from settings import Settings
 
 
@@ -20,7 +20,21 @@ class AuthService:
 
     def google_auth(self, code: str):
         user_data = self.google_client.get_user_info(code=code)
-        self.user_repository.create_user(user_data)
+
+        if user := self.user_repository.get_user_by_email(email=user_data.email):
+            access_token = self.generate_access_token(user_id=user.id)
+            print("user_login")
+            return UserLoginSchema(user_id=user.id, access_token=access_token)
+
+        create_user_data = UserCreateSchema(
+            google_access_token=user_data.access_token,
+            email=user_data.email,
+            name=user_data.name
+        )
+        created_user = self.user_repository.create_user(create_user_data)
+        access_token = self.generate_access_token(user_id=created_user.id)
+        print("user_create")
+        return UserLoginSchema(user_id=created_user.id, access_token=access_token)
 
     def get_google_redirect_url(self) -> str:
         return self.settings.google_redirect_url
